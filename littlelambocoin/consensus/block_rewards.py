6a0d34e9e5f8b68f35e93c31da49eaca444e3a47
BTCgreen-Network <svginsomnia@gmail.com>
2022-01-07 02:35:12 -0500

    100% reward NFT plot

--- littlelambocoin/consensus/block_rewards.py
+++ littlelambocoin/consensus/block_rewards.py
@@ -7,12 +7,13 @@ _blocks_per_year = 1681920  # 32 * 6 * 24 * 365
 
 def calculate_pool_reward(height: uint32) -> uint64:
     """
-    Returns the pool reward at a certain block height. The pool earns 7/8 of the reward in each block. If the farmer
+    Returns the pool reward at a certain block height. The pool earns 0/8 of the reward in each block. If the farmer
     is solo farming, they act as the pool, and therefore earn the entire block reward.
     These halving events will not be hit at the exact times
     (3 years, etc), due to fluctuations in difficulty. They will likely come early, if the network space and VDF
     rates increase continuously.
     """
+    return uint64(int(0))
 
     if height == 0:
         return uint64(int((7 / 8) * 5000000 * _byte_per_littlelambocoin))
@@ -27,28 +28,26 @@ def calculate_pool_reward(height: uint32) -> uint64:
     else:
         return uint64(int((7 / 8) * 0.5 * _byte_per_littlelambocoin))
 
-
 def calculate_base_farmer_reward(height: uint32) -> uint64:
     """
     Returns the base farmer reward at a certain block height.
-    The base fee reward is 1/8 of total block reward
-
+    The base fee reward is 8/8 of total block reward
     Returns the coinbase reward at a certain block height. These halving events will not be hit at the exact times
     (3 years, etc), due to fluctuations in difficulty. They will likely come early, if the network space and VDF
     rates increase continuously.
     """
     if height == 0:
-        return uint64(int((1 / 8) * 5000000 * _byte_per_littlelambocoin))
+        return uint64(5000000 * _byte_per_littlelambocoin)
     elif height < 3 * _blocks_per_year:
-        return uint64(int((1 / 8) * 8 * _byte_per_littlelambocoin))
+        return uint64(8 * _byte_per_littlelambocoin)
     elif height < 6 * _blocks_per_year:
-        return uint64(int((1 / 8) * 4 * _byte_per_littlelambocoin))
+        return uint64(4 * _byte_per_littlelambocoin)
     elif height < 9 * _blocks_per_year:
-        return uint64(int((1 / 8) * 2 * _byte_per_littlelambocoin))
+        return uint64(2 * _byte_per_littlelambocoin)
     elif height < 12 * _blocks_per_year:
-        return uint64(int((1 / 8) * 1 * _byte_per_littlelambocoin))
+        return uint64(1 * _byte_per_littlelambocoin)
     else:
-        return uint64(int((1 / 8) * 0.5 * _byte_per_littlelambocoin))
+        return uint64(0.5 * _byte_per_littlelambocoin)
 
 
 def calculate_base_timelord_fee(height: uint32) -> uint64:
@@ -59,14 +58,14 @@ def calculate_base_timelord_fee(height: uint32) -> uint64:
     rates increase continuously !!
     """
     if height == 0:
-        return uint64(int((1 / 1000) * 3500000 * _mojo_per_littlelambocoin))
+        return uint64(int((1 / 1000) * 3500000 * _byte_per_littlelambocoin))
     elif height < 3 * _blocks_per_year:
-        return uint64(int((1 / 1000) * 2 * _mojo_per_littlelambocoin))
+        return uint64(int((1 / 1000) * 2 * _byte_per_littlelambocoin))
     elif height < 6 * _blocks_per_year:
-        return uint64(int((1 / 1000) * 1 * _mojo_per_littlelambocoin))
+        return uint64(int((1 / 1000) * 1 * _byte_per_littlelambocoin))
     elif height < 9 * _blocks_per_year:
-        return uint64(int((1 / 1000) * 0.5 * _mojo_per_littlelambocoin))
+        return uint64(int((1 / 1000) * 0.5 * _byte_per_littlelambocoin))
     elif height < 12 * _blocks_per_year:
-        return uint64(int((1 / 1000) * 0.25 * _mojo_per_littlelambocoin))
+        return uint64(int((1 / 1000) * 0.25 * _byte_per_littlelambocoin))
     else:
-        return uint64(int((1 / 1000) * 0.125 * _mojo_per_littlelambocoin))
+        return uint64(int((1 / 1000) * 0.125 * _byte_per_littlelambocoin))
